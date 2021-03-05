1c845a10b4c1b41d3bc351ea570449bc3f62b808
Heatao <hetao_zj@foxmail.com>
2021-03-05 14:29:15 +0800

    checkstyle

--- hetu-clickhouse/src/main/java/io/hetu/core/plugin/clickhouse/ClickHouseClient.java
+++ hetu-clickhouse/src/main/java/io/hetu/core/plugin/clickhouse/ClickHouseClient.java
@@ -57,7 +57,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.TreeMap;
 import java.util.function.BiFunction;
 
 import static io.prestosql.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
